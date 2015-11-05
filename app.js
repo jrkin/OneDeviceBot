8aafb669ec15f861a1e23230f3e4f5eeef6be22c
jrkin <andrew.jrkin@gmail.com>
2015-11-05 20:47:36 +0300

    3rd

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var http =require ('http');
-http.createServer (function (req,res){
+var server_ip ='127.0.0.1';
+var server_port= 1337;
+if (process.env.NODE_ENV =='production')
+{
+	server_ip ='130.211.89.118';
+}
+ http.createServer (function (req,res){
 	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end('Hello World\n');
+	res.end('Hello World\n You\'re @ '& process.env.NODE_ENV & 'instance');
 
-}).listen(1337,'127.0.0.1');
-console.log('Server running at xxx');
+}).listen(server_port,server_ip);
+console.log('Server running at ' & server_ip &' port:'& server_port);
